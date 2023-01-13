da7cc2753c3c1ea80e8152a948c7a0b859b6ea1d
Leandro Santiago <leandro.santiago@pebmed.com.br>
2023-01-13 12:30:18 -0300

    overrides String.prototype.format parameters to ensure posibilities to work

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,50 @@
+interface Context {
+    view: any;
+    parent: Context | undefined;
+
+    /**
+     * Initializes a new instance of the {@link Context `Context`} class.
+     */
+    new(view: any, parentContext?: Context);
+
+    /**
+     * Creates a new context using the given view with this context as the parent.
+     *
+     * @param view
+     * The view to create the new context with.
+     */
+    push(view: any): Context;
+
+    /**
+     * Returns the value of the given name in this context, traversing up the context hierarchy if the value is absent in this context's view.
+     *
+     * @param name
+     * The name to look up.
+     */
+    lookup(name: string): any;
+}
+type PartialLookupFn = (partialName: string) => string | undefined;
+
+type PartialsOrLookupFn = Record<string, string> | PartialLookupFn;
+
+type OpeningAndClosingTags = [string, string];
+
+type RenderOptions = {
+    escape?: EscapeFunction | undefined;
+    tags?: OpeningAndClosingTags | undefined;
+}
+
 interface String {
+  format(template: string): string
+  format(template: string, props: {
+    view: any | Context,
+    partials?: PartialsOrLookupFn,
+    config?: OpeningAndClosingTags | RenderOptions
+  }): string
   format(
     template: string,
-    view: any | import('mustache').Context,
-    partials?: import('mustache').PartialsOrLookupFn,
-    tagsOrOptions?: import('mustache').OpeningAndClosingTags | RenderOptions,
+    view: any | Context,
+    partials?: PartialsOrLookupFn,
+    config?: OpeningAndClosingTags | RenderOptions,
   ): string
 }
--- index.js
+++ index.js
@@ -7,9 +7,30 @@
 }(this, (function () {
   'use strict';
 
-  String.prototype.format = function (template, view, partials, config) {
-    if (!view) view = { value: this }
-    else if (typeof view === 'object' && !view.value) view.value = this
+  const isObj = object => typeof object === 'object'
+
+  String.prototype.format = function () {
+    let template = arguments[0]
+    let view = { value: this }
+    let partials
+    let config
+
+    if (arguments.length === 2) {
+      let props = arguments[1]
+
+      if (!isObj(props)) throw new TypeError('props must be a object')
+      else if (!isObj(props.view)) throw new TypeError('props.view must be a object')
+
+      view = { ...view, ...props.view }
+      partials = props.partials
+      config = props.config
+    } else {
+      if (!isObj(arguments[1])) throw new TypeError('view must be a object')
+      view = { ...view, ...arguments[1] }
+      partials = arguments[2]
+      config = arguments[3]
+    }
+
     return require('mustache').render(template, view, partials, config)
   }
 })))
